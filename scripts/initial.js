6cc528487f5020ed6b51f8bc2e14850483599803
Rj Bernaldo <rj.bernaldo@gmail.com>
2016-04-30 20:17:18 +0800

    Add test command

--- scripts/initial.js
+++ scripts/initial.js
@@ -1,7 +1,11 @@
 module.exports = initial
 
 function initial(robot) {
-	robot.hear(/test/i, function(res) {
-		res.send('test');
+	robot.hear(/deckard/i, function(res) {
+		res.send('woof!!');
+	});
+
+	robot.hear(/.deckard hi/i, function(res) {
+		res.send('https://scontent.fmnl2-1.fna.fbcdn.net/t39.1997-6/p64x64/851556_499671063448730_199601500_n.png');
 	});
 }
