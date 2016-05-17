f60a7349d1371f5aa0b12ae5afa35d0865210a31
Rj Bernaldo <rj.bernaldo@gmail.com>
2016-05-17 19:57:38 +0800

    Add existing kissing countdown function from Pandesal to Deckard

--- scripts/first.js
+++ scripts/first.js
@@ -1,7 +1,14 @@
 module.exports = first
 
 function first(robot) {
-  robot.hear(/test/i, function(res) {
-    res.send('I\'m here!');
+  robot.hear(/kissing countdown/i, function(res) {
+    var arrival = new Date('June 10 2016').getTime();
+    var now = new Date().getTime();
+    var daysLeft = Math.floor((arrival - now) / (1000 * 60 * 60 * 24));
+    res.send(daysLeft + ' until it\'s kissing time for Taco and Pretzel');
+  });
+
+  robot.hear(/pandesal/i, function(res) {
+    res.send('Who cares about Pandesal? No offense but I think I\'m the better assistant');
   });
 }
