f1897d286c7769d00caca05694b54b042942b567
Rj Bernaldo <rj@promisepay.com>
2016-06-19 16:04:41 -0700

    Update countdown script

--- scripts/first.js
+++ scripts/first.js
@@ -6,10 +6,11 @@ module.exports = first
 
 function first(robot) {
   robot.hear(/kissing countdown/i, function(res) {
-    var arrival = new Date('June 10 2016').getTime();
-    var now = new Date().getTime();
-    var daysLeft = Math.floor((arrival - now) / (1000 * 60 * 60 * 24));
-    res.send(daysLeft + ' days until it\'s kissing time for Taco and Pretzel');
+    // var arrival = new Date('June 10 2016').getTime();
+    // var now = new Date().getTime();
+    // var daysLeft = Math.floor((arrival - now) / (1000 * 60 * 60 * 24));
+    // res.send(daysLeft + ' days until it\'s kissing time for Taco and Pretzel');
+    res.send('When is it kissing time for Taco and Pretzel? NOW.');
   });
 
   robot.hear(/pandesal/i, function(res) {
